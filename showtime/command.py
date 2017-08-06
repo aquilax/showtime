92cf727a12890c5c7e1527ec3384e206c28386ae
aquilax <aquilax@gmail.com>
2017-08-06 08:43:50 +0200

    Added barebones functionality

--- showtime/command.py
+++ showtime/command.py
@@ -1,11 +1,6 @@
 from cmd2 import Cmd
 from tvmaze.api import Api
-
-class Database():
-
-    def add(self, show):
-        print(show)
-
+from database import Database
 
 class Showtime(Cmd):
 
@@ -17,21 +12,53 @@ class Showtime(Cmd):
     def do_search(self, query):
         search_result = api.search.shows(query)
         for show in search_result:
-            print('({id}) {name} - {permiere}'.format(
-                id=show.id, name=show.name, permiere=show.premiered))
+            print('({id}) {name} - {permiered} {url}'.format(
+                id=show.id, name=show.name, permiered=show.premiered,
+                url=show.url))
 
     def do_add(self, show_id):
         show = api.show.get(show_id)
         self.db.add(show)
+        print('Added show: ({id}) {name} - {permiered}'.format(
+                id=show.id, name=show.name, permiered=show.premiered))
 
     def do_episodes(self, show_id):
-        episodes = api.show.episodes(show_id)
+        show = db.get_show(int(show_id))
+        episodes = db.get_episodes(int(show_id))
+        print('Episodes for ({id}) {name} - {permiered}'.format(
+                id=show['id'], name=show['name'], permiered=show['permiered']))
+        for episode in episodes:
+            watched = ''
+            if 'watched' in episode:
+                watched = episode['watched']
+            print('S{season:0>2} E{episode:0>2} ({id}) {name} - {airdate} : {watched}'.format(
+                season=episode['season'], episode=episode['number'],
+                id=episode['id'], name=episode['name'],
+                airdate=episode['airdate'], watched=watched))
+
+    def do_sync(self, line):
+        print('Syncing shows...')
+        shows = self.db.get_shows()
+        for show in shows:
+            print('({id}) {name} - {permiered}'.format(
+                id=show['id'], name=show['name'], permiered=show['permiered']))
+            episodes = self.api.show.episodes(show['id'])
+            self.db.sync_episodes(show['id'], episodes);
+        print('Done')
+
+    def do_watched(self, episode_id):
+        self.db.mark_watched(int(episode_id))
+
+    def do_unwatched(self, line):
+        episodes = self.db.get_unwatched()
         for episode in episodes:
             print('S{season:0>2} E{episode:0>2} ({id}) {name} - {airdate}'.format(
-                season=episode.season, episode=episode.number, id=episode.id,
-                name=episode.name, airdate=episode.airdate))
+                season=episode['season'], episode=episode['number'],
+                id=episode['id'], name=episode['name'],
+                airdate=episode['airdate']))
+
 
 if __name__ == '__main__':
     api = Api()
-    db = Database()
+    db = Database('/tmp/temp.json')
     Showtime(api, db).cmdloop()
---
+++ showtime/database.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+import os
+import json
+from tinydb import TinyDB, Query
+
+class Database():
+
+    db = None
+
+    def __init__(self, file_name):
+        self.db = TinyDB(file_name)
+        self.show_table = self.db.table('show')
+        self.episode_table = self.db.table('episode')
+
+    def add(self, show):
+        Show = Query()
+        if not self.show_table.contains(Show.id == show.id):
+            self.show_table.insert({
+                'id': show.id,
+                'name': show.name,
+                'permiered': show.premiered
+            })
+        return show.id
+
+    def get_shows(self):
+        return self.show_table.all()
+
+    def get_show(self, show_id: int):
+        Show = Query()
+        return self.show_table.get(Show.id == show_id)
+
+    def get_episodes(self, show_id):
+        Episode = Query()
+        return self.episode_table.search(Episode.show_id == show_id)
+
+    def sync_episodes(self, show_id, episodes):
+        exising_episodes = self.get_episodes(show_id)
+        for episode in episodes:
+            matched = [x for x in exising_episodes if x['id'] == episode.id]
+            if not matched:
+                print('Adding new episode: S{season:0>2} E{episode:0>2} ({id}) {name} - {airdate}'.format(
+                    season=episode.season, episode=episode.number, id=episode.id,
+                    name=episode.name, airdate=episode.airdate))
+
+                self.episode_table.insert({
+                    'id': episode.id,
+                    'show_id': show_id,
+                    'season': episode.season,
+                    'number': episode.number,
+                    'name': episode.name,
+                    'airdate': episode.airdate,
+                    'runtime': episode.runtime,
+                    'watched': ''
+                })
+
+    def mark_watched(self, episode_id: int):
+        Episode = Query()
+        self.episode_table.update({
+            'watched': datetime.utcnow().isoformat()
+        }, Episode.id == episode_id)
+
+    def get_unwatched(self):
+        Episode = Query()
+        return self.episode_table.search(Episode.watched == '')
