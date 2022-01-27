fcdf13414481b34e4ee8a0cb31f6bfaec934ea9f
aquilax <aquilax@gmail.com>
2022-01-27 18:29:04 +0100

    Fixes config test in ci

--- tests/config_test.py
+++ tests/config_test.py
@@ -17,8 +17,3 @@ def test_config() -> Config:
 def test_load_file_name(test_config):
     test_config.load('test_file_name.ini')
     test_config.read.assert_called_once_with('test_file_name.ini')
-
-
-def test_load_default(test_config):
-    test_config.load('')
-    test_config.read.assert_called_once()
